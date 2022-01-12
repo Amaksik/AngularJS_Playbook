2ea5d35da377f3da51d2487a5e94c0bb91e52809
Amaksik <a.maksik2013@gmail.com>
2022-01-12 16:49:49 +0200

    add basik component

--- myApp/lib/angular.js
+++ myApp/lib/angular.js
@@ -1,2 +1,20 @@
-var app = angular.module('myApp', ["ngRoute"]);
+
+
+(function() {
+  'use strict';
+
+  var app = angular.module('myApp', ["ngRoute"]);
+
+  app.component("sidebarWithMenu",{
+    template: '<p>Hello World and {{$ctrl.name}} ! I`m {{$ctrl.myName}}! </p>',
+    controller: function() {},
+    bindings: {
+      name: '<'
+    },
+      controller: function(){
+        this.myName = 'Alain';
+      }
+})
+
+})();
 
